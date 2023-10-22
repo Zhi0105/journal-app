3414b57699f257148e4adefcdc2e03dca8317143
Argie <argie.b@flowerstore.ph>
2023-10-22 23:08:16 +0800

    get all tasks

--- src/providers/TaskProvider.tsx
+++ src/providers/TaskProvider.tsx
@@ -1,12 +1,17 @@
 "use client"
+import { useCallback } from "react";
 import { TaskContext } from "@/contexts/TaskContext"
 import { taskInterface } from "@/types/task/interface"
-import { CreateTask } from "@/services/task";
+import { GetTask, CreateTask } from "@/services/task";
 import { useQuery, useMutation, useQueryClient  } from "@tanstack/react-query";
+import { useUserStore } from "@/store/auth";
+import { UseTaskStore } from "@/store/tasl";
 import { useRouter } from "next/navigation"
 import { toast } from "react-toastify"
 
 export const TaskProvider = ({ children }: { children: React.ReactNode }) => {
+  const { token } = useUserStore((state) => ({ token: state.token }));
+  const { setTasks } = UseTaskStore((state) => ({ setTasks: state.setTasks }));
   const queryClient = useQueryClient();
   const router = useRouter()
 
@@ -22,6 +27,24 @@ export const TaskProvider = ({ children }: { children: React.ReactNode }) => {
       toast(err.response.data.message, { type: "warning" })
     },
   });
+
+  
+ //  GET TASKS REQUEST
+useQuery({
+  queryKey: ["task"],
+  queryFn: () => handleTask()
+})
+
+const handleTask:any = useCallback(async () => {
+  if(token) {
+    const tasks = await GetTask(token)
+    if(tasks){
+      setTasks(tasks)
+      return tasks
+    }
+  }
+}, [token, setTasks])
+
 
   return (
     <TaskContext.Provider
