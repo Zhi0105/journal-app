0621736ff49d0ec598f2f3741f70be5e1e4197dd
Argie <argie.b@flowerstore.ph>
2023-10-28 19:10:26 +0800

    task update

--- src/components/Category/View.tsx
+++ src/components/Category/View.tsx
@@ -1,16 +1,18 @@
 
-import { FC, useEffect } from "react"
+import { FC, useState } from "react"
 import { categoryItemInterface } from "@/types/category/interface"
 import { PiMagnifyingGlassDuotone } from 'react-icons/pi'
 import { UseTaskStore } from "@/store/task";
 import { taskItemInterface } from "@/types/task/interface";
-
+import { getCategoryTasks } from "@/helpers/helpers";
 interface ReadCategoryInterface {
   category: categoryItemInterface
 }
 
 export const View:FC<ReadCategoryInterface> = ({ category }) => {
   const { tasks } = UseTaskStore((state) => ({ tasks: state.tasks }));
+  const [taskList, setTaskList] = useState<taskItemInterface[]>(getCategoryTasks(tasks, category.id))
+
   return (
     <div className="category_detail_main w-full p-4 bg-gray-100">
       <div className='seach w-full'>
@@ -32,11 +34,11 @@ export const View:FC<ReadCategoryInterface> = ({ category }) => {
       <div className="w-full mt-5 p-6 bg-green-100 border border-gray-200 rounded-lg shadow">
         <div className="flex justify-between px-2"> 
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">To do</h5>
-          <h1 className="text-gray-400">{tasks.length} result(s)</h1>
+          <h1 className="text-gray-400">{taskList.length} result(s)</h1>
         </div>
 
         <div className="max-h-96 overflow-x-auto flex flex-col gap-4 p-4">
-          {tasks.length && tasks.map((task: taskItemInterface, index: number) => {
+          {taskList.length && taskList.map((task: taskItemInterface, index: number) => {
             return (
               <div key={index} className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <h1 className="font-bold text-lg">{task.name}</h1>
