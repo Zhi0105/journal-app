afb33f7a19ae17175c3eb74ead23d79155e62b5d
Argie <argie.b@flowerstore.ph>
2023-11-14 13:29:50 +0800

    remove welcome json

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -6,7 +6,7 @@ import { UseTaskStore } from "@/store/task";
 import { Calendar } from "./Partials/calendar/Calendar";
 import Lottie from "lottie-react";
 import hand from '@_assets/hand.json'
-import welcome from '@_assets/welcome.json'
+// import welcome from '@_assets/welcome.json'
 
 
 
@@ -30,22 +30,22 @@ export const Panel = () => {
   }, [user])
 
 
-  const EmptyCalendar = () => {
-    return (
-      <div className="w-full grid grid-cols-1 place-items-center">
-        <div className="flex xs:flex-col sm:flex-col md:flex-row lg-flex-row xl:flex-row justify-center items-center gap-2">
-          <div className="xs:w-full sm:w-full md:w-3/6 lg:w-3/6 w-3/6 flex flex-col gap-1 xs:order-2 sm:order-2 md:order-1 lg:order-1 xl:order-1 justify-center">
-            <p>Hello from <span className="text-blue-600 font-bold">Filofax</span>,</p>
-            <p className="indent-2">Create your first journal now!, kindly click the journal on the side navigator to create your first journal and start managing your tasks per category.</p>
-            <p className="indent-2">After creating your category, create your first task for the category that you have been created. Enjoy!</p>
-          </div>
-          <div className="xs:order-1 sm:order-1 md:order-2 lg:order-2 xl:order-2" style={{ width:"40%", height: "40%" }}>
-            <Lottie animationData={welcome}/>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  // const EmptyCalendar = () => {
+  //   return (
+  //     <div className="w-full grid grid-cols-1 place-items-center">
+  //       <div className="flex xs:flex-col sm:flex-col md:flex-row lg-flex-row xl:flex-row justify-center items-center gap-2">
+  //         <div className="xs:w-full sm:w-full md:w-3/6 lg:w-3/6 w-3/6 flex flex-col gap-1 xs:order-2 sm:order-2 md:order-1 lg:order-1 xl:order-1 justify-center">
+  //           <p>Hello from <span className="text-blue-600 font-bold">Filofax</span>,</p>
+  //           <p className="indent-2">Create your first journal now!, kindly click the journal on the side navigator to create your first journal and start managing your tasks per category.</p>
+  //           <p className="indent-2">After creating your category, create your first task for the category that you have been created. Enjoy!</p>
+  //         </div>
+  //         <div className="xs:order-1 sm:order-1 md:order-2 lg:order-2 xl:order-2" style={{ width:"40%", height: "40%" }}>
+  //           <Lottie animationData={welcome}/>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 
   return (
     <div className="dashboard_main w-full p-2">
@@ -103,11 +103,15 @@ export const Panel = () => {
           </div>
         }
       <div className="px-8 mt-5">
-        {tasks.length != 0 ?
+        {tasks.length != 0 &&
+          <Calendar categories={categories} tasks={tasks}/>
+          
+        }
+        {/* {tasks.length != 0 ?
             <Calendar categories={categories} tasks={tasks}/>
           :
             <EmptyCalendar />
-        }
+        } */}
       </div>
     </div>
   )
